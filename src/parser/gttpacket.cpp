c010c4f702d04dd32eee540896c492f6a695077f
Nicolas Iooss <nicolas.iooss@polytechnique.org>
2012-06-15 15:15:53 +0200

    Packet basic implementation
    Signed-off-by: Nicolas Iooss <nicolas.iooss@polytechnique.org>

---
+++ src/parser/gttpacket.cpp
@@ -0,0 +1,57 @@
+#include "gttpacket.h"
+#include "../core/common.h"
+#include <sstream>
+#include <string.h>
+
+namespace iom
+{
+
+    GTTPacket::GTTPacket()
+    :size(0), body(NULL) {
+    }
+
+    GTTPacket::~GTTPacket() {
+        if (body)
+            delete[] body;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const GTTPacket& pkt) {
+        os << "[Proto " << pkt.protocol << " method " << pkt.method << std::endl;
+        for (std::map<std::string, std::string>::const_iterator i = pkt.headers.begin();
+            i != pkt.headers.end(); ++i) {
+            os << "  " << (*i).first << ": " << (*i).second << std::endl;
+        }
+        if (pkt.size > 0) {
+            os << "  content-length: " << pkt.size << std::endl;
+        }
+        os << "]";
+        return os;
+    }
+
+    unsigned long GTTPacket::build(char **newData) const {
+        BOOST_ASSERT(newData != NULL);
+        std::stringstream head;
+        std::map<std::string, std::string>::const_iterator it;
+
+        // First line
+        head << protocol << " " << method << String::crlf;
+
+        // Headers
+        for (it = headers.begin(); it != headers.end(); it++) {
+            head << it->first << ": " << it->second << String::crlf;
+        }
+        if (size > 0) {
+            head << "content-length: " << size << String::crlf;
+        }
+        head << String::crlf;
+
+        // Concatenate headers and content
+        const std::string str = head.str();
+        unsigned long strSize = str.length();
+        char *data = new char[strSize + size];
+        strncpy(data, str.c_str(), strSize);
+        memcpy(data + strSize, body, size);
+        *newData = data;
+        return strSize + size;
+    }
+}
---
+++ src/parser/gttpacket.h
@@ -0,0 +1,70 @@
+/**
+ * @file gttpacket.h
+ * @brief Packet of the Generic Text Template
+ */
+
+#ifndef IOM_PARSER_GTTPACKET_H
+#define IOM_PARSER_GTTPACKET_H
+
+#include <map>
+#include <string>
+#include <ostream>
+#include "../net/socket.h"
+
+namespace iom
+{
+    /**
+     * @class GTTPacket
+     *
+     * @brief Packet for Generic Text Template
+     */
+    class GTTPacket
+    {
+    public:
+        /**
+         * @brief Constructor
+         */
+        GTTPacket();
+
+        ~GTTPacket();
+
+        /**
+         * @brief Print a short packet desciption in an output stream (for debug)
+         * @param os output stream
+         * @param pkt
+         */
+        friend std::ostream& operator<<(std::ostream& os, const GTTPacket& pkt);
+
+        /**
+         * @brief Build the raw text query for this GTT Packet
+         * @param newData newly-allocated buffer with ready-to-send data
+         * @return size of *data
+         *
+         * Note: Please delete *newData with delete[]
+         */
+        unsigned long build(char **newData) const;
+
+        /**
+         * @brief Protocol name
+         */
+        std::string protocol;
+        /**
+         * @brief Method name
+         */
+        std::string method;
+        /**
+         * @brief Headers map
+         */
+        std::map<std::string, std::string> headers;
+        /**
+         * @brief Content size
+         */
+        unsigned long size;
+        /**
+         * @brief Content body
+         */
+        const char *body;
+    };
+}
+
+#endif /* IOM_PARSER_GTTPACKET_H */
